f17de575c2d10dda6b602f5d0b784123aa78e545
Claude Ihany <63082098+C-Iahny@users.noreply.github.com>
2023-02-04 22:55:30 +0100

    Part_001

---
+++ mksd/forms.py
@@ -0,0 +1,64 @@
+from django import forms 
+from .models import Post, Category, Comment
+
+
+#choices = [('sport', 'sport'), ('kultur', 'kultur')]
+choices = Category.objects.all().values_list('name', 'name')
+
+choice_list = []
+
+for item in choices:
+	choice_list.append(item)
+
+class PostForm(forms.ModelForm):
+	class Meta:
+		model = Post 
+		fields = ('title', 'author', 'category', 'body', 'snippet', 'header_image')
+
+		widgets = {
+
+			'title': forms.TextInput(attrs={'class': 'form-control'}),
+			#'author': forms.Select(attrs={'class': 'form-control'}),
+			'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'Ihany', 'type': 'hidden'}),
+			'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+			'body': forms.Textarea(attrs={'class': 'form-control'}),
+			'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+
+		}
+
+
+
+class EditForm(forms.ModelForm):
+	class Meta:
+		model = Post 
+		fields = ('title', 'author', 'body', 'snippet')
+
+		widgets = {
+
+			'title': forms.TextInput(attrs={'class': 'form-control'}),
+			'author': forms.Select(attrs={'class': 'form-control'}),
+			'body': forms.Textarea(attrs={'class': 'form-control'}),
+			'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+
+		}
+
+
+class CommentForm(forms.ModelForm):
+	class Meta:
+		model = Comment 
+		fields = ('name', 'body')
+
+		widgets = {
+			'name': forms.TextInput(attrs={'class': 'form-control'}),
+			'body': forms.Textarea(attrs={'class': 'form-control'}),
+		} 
+
+
+
+
+
+
+
+
+
+
