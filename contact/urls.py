f78437d6c94e262c9fff52c671e11b0ee4c03d49
Claude Ihany <63082098+C-Iahny@users.noreply.github.com>
2023-03-06 21:34:27 +0100

    Update urls.py

--- contact/urls.py
+++ contact/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 	path('about/', About.as_view(), name="about"), 
 	path('impressum/', Impressum.as_view(), name="impressum"), 
 	path('anmeldebogen/', Anmeldebogen, name="anmeldebogen"), 
-	path('faq/', views.Faq, name="faq"),
+	path('faq/', Faq, name="faq"),
 
 
 ]
