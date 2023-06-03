6156006897a8cd60e6851771980be78626a6067b
Claude Ihany <63082098+C-Iahny@users.noreply.github.com>
2023-06-03 14:42:44 +0200

    Ultim_update_of_03_06_23

--- album/admin.py
+++ album/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Album, Images, FileUpload
 
-# Register your models here.
+
+admin.site.register(Album)
+admin.site.register(Images)
+admin.site.register(FileUpload)
+
