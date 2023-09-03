de1b8dc478ca1fe72fc9c6046cebd1f3e587b709
Claude Ihany <63082098+C-Iahny@users.noreply.github.com>
2023-09-03 20:52:55 +0200

    Update settings.py

--- munich/settings.py
+++ munich/settings.py
@@ -156,11 +156,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')],
+
 
 
 CSRF_TRUSTED_ORIGINS = ['https://*.','https://*.127.0.0.1']
