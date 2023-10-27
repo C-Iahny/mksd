921c1e008216763ad42753d5e7213c90dcc13010
Claude Ihany <63082098+C-Iahny@users.noreply.github.com>
2023-10-27 11:19:28 +0200

    Update settings.py

--- munich/settings.py
+++ munich/settings.py
@@ -83,15 +83,15 @@ WSGI_APPLICATION = 'munich.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -102,7 +102,7 @@ DATABASES = {
         'PORT': '6598',
     }
 }
-
+"""
 
 """
 ID = Admin_mksd
